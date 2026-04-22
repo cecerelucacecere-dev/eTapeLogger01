1fdf23ae1afb2b7cdcf3c6a2c77488c0f45986c7
Luca <cecere.luca.cecere@gmail.com>
2026-04-22 10:49:10 -0500

    CHanged max charge current

--- src/eTapeLogger01.ino
+++ src/eTapeLogger01.ino
@@ -213,9 +213,9 @@ void goToSleep() {
 void battSettings() {
   
   SystemPowerConfiguration conf; 
-  conf.powerSourceMaxCurrent(900)    // 5W / 5V = 1000mA. 900mA is the closest PMIC register setting.
+  conf.powerSourceMaxCurrent(500)    // 5W / 5V = 1000mA. 900mA is the closest PMIC register setting.
       .powerSourceMinVoltage(3880)  
-      .batteryChargeCurrent(150)
+      .batteryChargeCurrent(500)
       .batteryChargeVoltage(4110);  
       
   System.setPowerConfiguration(conf);
